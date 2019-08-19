8c614b9b7e98af12d0f9bda555f532bc24fedf92
Maciej Kędra <maciejken@gmail.com>
2019-08-19 14:19:59 +0200

    remove unnecessary CardSection import in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button, Spinner, CardSection } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import { LoginForm } from './components';
 
 class App extends Component {
@@ -38,7 +38,7 @@ class App extends Component {
             case false:
                 return <LoginForm />;
             default:
-                return <Spinner size="large" />;
+                return <Spinner />;
         }
     }
 
